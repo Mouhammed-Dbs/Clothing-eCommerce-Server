03c352042115c12794514a38ac55ad0cc1daed1e
Mouhammed Dbs <mouhammeddbs@gmail.com>
2024-08-12 19:48:09 +0300

    format

--- services/productService.js
+++ services/productService.js
@@ -1,18 +1,18 @@
-const asyncHandler = require('express-async-handler');
-const { v4: uuidv4 } = require('uuid');
-const sharp = require('sharp');
+const asyncHandler = require("express-async-handler");
+const { v4: uuidv4 } = require("uuid");
+const sharp = require("sharp");
 
-const { uploadMixOfImages } = require('../middlewares/uploadImageMiddleware');
-const factory = require('./handlersFactory');
-const Product = require('../models/productModel');
+const { uploadMixOfImages } = require("../middlewares/uploadImageMiddleware");
+const factory = require("./handlersFactory");
+const Product = require("../models/productModel");
 
 exports.uploadProductImages = uploadMixOfImages([
   {
-    name: 'imageCover',
+    name: "imageCover",
     maxCount: 1,
   },
   {
-    name: 'images',
+    name: "images",
     maxCount: 5,
   },
 ]);
@@ -24,8 +24,8 @@ exports.resizeProductImages = asyncHandler(async (req, res, next) => {
     const imageCoverFileName = `product-${uuidv4()}-${Date.now()}-cover.jpeg`;
 
     await sharp(req.files.imageCover[0].buffer)
-      .resize(2000, 1333)
-      .toFormat('jpeg')
+      .resize(2000, 4000)
+      .toFormat("jpeg")
       .jpeg({ quality: 95 })
       .toFile(`uploads/products/${imageCoverFileName}`);
 
@@ -40,8 +40,8 @@ exports.resizeProductImages = asyncHandler(async (req, res, next) => {
         const imageName = `product-${uuidv4()}-${Date.now()}-${index + 1}.jpeg`;
 
         await sharp(img.buffer)
-          .resize(2000, 1333)
-          .toFormat('jpeg')
+          .resize(2000, 4000)
+          .toFormat("jpeg")
           .jpeg({ quality: 95 })
           .toFile(`uploads/products/${imageName}`);
 
@@ -57,12 +57,12 @@ exports.resizeProductImages = asyncHandler(async (req, res, next) => {
 // @desc    Get list of products
 // @route   GET /api/v1/products
 // @access  Public
-exports.getProducts = factory.getAll(Product, 'Products');
+exports.getProducts = factory.getAll(Product, "Products");
 
 // @desc    Get specific product by id
 // @route   GET /api/v1/products/:id
 // @access  Public
-exports.getProduct = factory.getOne(Product, 'reviews');
+exports.getProduct = factory.getOne(Product, "reviews");
 
 // @desc    Create product
 // @route   POST  /api/v1/products
