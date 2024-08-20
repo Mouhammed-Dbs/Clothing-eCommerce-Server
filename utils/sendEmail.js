a94191038dd027dc05bd0bff43dc44f3d092de54
Mouhammed Dbs <mouhammeddbs@gmail.com>
2024-08-20 18:41:35 +0300

    fixing message

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -15,7 +15,7 @@ const sendEmail = async (options) => {
 
   // 2) Define email options (like from, to, subject, email content)
   const mailOpts = {
-    from: "E-shop App <info@saramoda.shop>",
+    from: "Saramoda App <info@saramoda.shop>",
     to: options.email,
     subject: options.subject,
     text: options.message,
